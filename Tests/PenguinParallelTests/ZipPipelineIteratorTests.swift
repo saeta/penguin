bf110ed78b50247f2da9193d9b8107f35cff090c
Brennan Saeta <saeta@google.com>
2019-12-24 21:32:40 -0800

    Add test to verify correct early termination behavior.

--- Tests/PenguinParallelTests/ZipPipelineIteratorTests.swift
+++ Tests/PenguinParallelTests/ZipPipelineIteratorTests.swift
@@ -5,7 +5,7 @@ final class ZipPipelineIteratorTests: XCTestCase {
 
     func testZipAndMapTwoArrays() {
         let arr = [0, 1, 2, 3, 4]
-        var tmp = PipelineIterator.zip(arr.makePipelineIterator(), arr.makePipelineIterator().map { $0 + 1 })
+        let tmp = PipelineIterator.zip(arr.makePipelineIterator(), arr.makePipelineIterator().map { $0 + 1 })
         var itr = tmp.map { $0.0 + $0.1 }
         XCTAssertEqual(1, try! itr.next())
         XCTAssertEqual(3, try! itr.next())
@@ -15,7 +15,15 @@ final class ZipPipelineIteratorTests: XCTestCase {
         XCTAssertEqual(nil, try! itr.next())
     }
 
+    func testZipEndEarly() {
+        var itr = PipelineIterator.zip([0, 1, 2].makePipelineIterator(), [0, 1].makePipelineIterator())
+        XCTAssert(try! itr.next() != nil)
+        XCTAssert(try! itr.next() != nil)
+        XCTAssert(try! itr.next() == nil)
+    }
+
     static var allTests = [
         ("testZipAndMapTwoArrays", testZipAndMapTwoArrays),
+        ("testZipEndEarly", testZipEndEarly),
     ]
 }
