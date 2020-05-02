462b3ca80cac21d60307af99996f563d2324490a
Brennan Saeta <saeta@google.com>
2020-05-01 20:42:48 -0700

    Allow arbitrary output sinks for topological sort.
    This change makes the topological sort implementation more generic
    by allowing an arbitrary way of consuming the output of the topological
    sort algorithm. In reverse topological order, we call `reverseSink`, a
    function of type `(Graph.VertexId) -> Void`.
    
    This change then reimplements the original topological sort in terms of
    the this newly refactored `topologicalSort` implementation.

--- Sources/PenguinGraphs/TopologicalSort.swift
+++ Sources/PenguinGraphs/TopologicalSort.swift
@@ -16,41 +16,56 @@ import PenguinStructures
 
 extension Graphs {
 
-  /// Computes a topological sort of `graph`.
+
+  /// Computes a [topological sort](https://en.wikipedia.org/wiki/Topological_sorting) of `graph`.
+  ///
+  /// - Parameter graph: the graph upon whose vertices the topological sort will be computed.
+  /// - Parameter reverseSink: this function will be called once for every vertex in reverse
+  ///   topological sort order.
+  /// - Throws: if a cycle is detected.
+  public static func topologicalSort<Graph: IncidenceGraph & VertexListGraph>(
+    _ graph: inout Graph,
+    reverseSink: (Graph.VertexId) -> Void
+  ) throws where Graph.VertexId: IdIndexable {
+    try withoutActuallyEscaping(reverseSink) { reverseSink in
+      var visitor = TopologicalSortVisitor<Graph>(reverseSink: reverseSink)
+      try Graphs.depthFirstTraversal(&graph, visitor: &visitor)
+    }
+  }
+
+  /// Computes a [topological sort](https://en.wikipedia.org/wiki/Topological_sorting) of `graph`.
   ///
   /// A topological sort means that for every 0 <= i < j < graph.vertexCount,
-  /// there does not exist an edge from returnValue[j] to returnValue[i] (i.e.
-  /// backwards in the array).
+  /// there does not exist an edge from `returnValue[j]` to `returnValue[i]`
+  /// (i.e. backwards in the array).
   ///
-  /// The implementation for `topologicalSort` is primarily a call to
-  /// `depthFirstTraversal`.
+  /// - Throws: if a cycle is detected.
   public static func topologicalSort<Graph: IncidenceGraph & VertexListGraph>(
     _ graph: inout Graph
   ) throws -> [Graph.VertexId] where Graph.VertexId: IdIndexable {
     let vertexCount = graph.vertexCount
     let output = try [Graph.VertexId](unsafeUninitializedCapacity: vertexCount) { buffer, filled in
-      var visitor = TopologicalSortVisitor<Graph>(
-        outputBuffer: buffer, nextAvailable: vertexCount - 1)
-      try Graphs.depthFirstTraversal(&graph, visitor: &visitor)
+      var ptr = buffer.baseAddress! + vertexCount
+
+      var visitor = TopologicalSortVisitor<Graph>{ vId in
+        ptr -= 1
+        ptr.initialize(to: vId)
+      }
+      try Graphs.depthFirstTraversal(&graph, visitor: &visitor)  // Memory leaked if VertexId non-trivial.
       filled = vertexCount
     }
     return output
   }
 }
 
-private struct TopologicalSortVisitor<Graph: GraphProtocol>: DFSVisitor
-where Graph.VertexId: IdIndexable {
-  let outputBuffer: UnsafeMutableBufferPointer<Graph.VertexId>
-  var nextAvailable: Int
+private struct TopologicalSortVisitor<Graph: GraphProtocol>: DFSVisitor {
+  let reverseSink: (Graph.VertexId) -> Void
 
   mutating func backEdge(_ edge: Graph.EdgeId, _ graph: inout Graph) throws {
     throw GraphErrors.cycleDetected
   }
 
   mutating func finish(vertex: Graph.VertexId, _ graph: inout Graph) {
-    assert(nextAvailable >= 0)
-    assert(nextAvailable < outputBuffer.count)
-    outputBuffer[nextAvailable] = vertex
-    nextAvailable -= 1
+    reverseSink(vertex)
   }
 }
