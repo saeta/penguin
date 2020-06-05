04592e34a8dc224259bede014c88c6f36ee833c4
Dave Abrahams <dabrahams@google.com>
2020-06-04 19:38:24 -0700

    Add FactoryInitializable

--- Tests/PenguinStructuresTests/XCTestManifests.swift
+++ Tests/PenguinStructuresTests/XCTestManifests.swift
@@ -24,6 +24,7 @@ import XCTest
       testCase(ArrayStorageTests.allTests),
       testCase(DequeTests.allTests),
       testCase(DoubleEndedBufferTests.allTests),
+      testCase(FactoryInitializableTests.allTests),
       testCase(FixedSizeArrayTests.allTests),
       testCase(TupleTests.allTests),
       testCase(HeapTests.allTests),
