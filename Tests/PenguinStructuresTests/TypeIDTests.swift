db071af041eba0f40d92d4835dd3d56618c683bc
Dave Abrahams <dabrahams@google.com>
2020-08-14 15:06:10 -0700

    Expose testing facilities as PenguinTesting

--- Tests/PenguinStructuresTests/TypeIDTests.swift
+++ Tests/PenguinStructuresTests/TypeIDTests.swift
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+import PenguinTesting
+
 @testable import PenguinStructures
 import XCTest
 
